75c3532c2b72c9f16ecef896bc4c156a47b913cb
AseelM <aseelmustafa@outlook.com>
2017-10-12 22:18:59 +0100

    created game genres obj #40

---
+++ src/model/genresObject.js
@@ -0,0 +1,19 @@
+var genres = {
+  1 : null,
+  2 : "Point-and-click",
+  3 : null,
+  4 : "Fighting",
+  5 : "Shooter",
+  6 : null,
+  7 : "Music",
+  8 : "Platform",
+  9 : "Puzzle",
+  10 : "Racing",
+  11 : "Real Time Strategy (RTS)",
+  12 : "Role-playing (RPG)",
+  13 : "Simulator",
+  14 : "Sport",
+  15 : "Strategy",
+  16 : "Turn-based strategy (TBS)"
+}
+module.exports = genres;
