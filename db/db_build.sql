25fe2477bd0348e4cb0c04e99f23ecdcd96e2c9b
AseelM <aseelmustafa@outlook.com>
2017-10-12 21:40:32 +0100

    fixed errors with schema data types #39

--- db/db_build.sql
+++ db/db_build.sql
@@ -11,7 +11,6 @@ CREATE TABLE users (
     newUser BOOLEAN DEFAULT 'false' NOT NULL,
     profile_url VARCHAR(8000) NOT NULL
 );
-https://api-2445582011268.apicast.io/games/?search=overwatch&fields=name,category,genres,game_modes,cover,first_release_date
 
 CREATE TABLE games (
     id SERIAL PRIMARY KEY,
@@ -19,8 +18,8 @@ CREATE TABLE games (
     category INT UNIQUE NOT NULL,
     genres VARCHAR(200) NOT NULL,
     game_modes VARCHAR(500) NOT NULL,
-    cover VARCHAR(8000) NOT NULL BOOLEAN DEFAULT 'false' NOT NULL,
-    first_release_date INT UNIQUE NOT NULL
+    cover VARCHAR(8000) NOT NULL,
+    first_release_date BIGINT UNIQUE NOT NULL
 );
 
 
@@ -29,11 +28,11 @@ INSERT INTO users (fb_id,name,email,avatar,newUser,profile_url) VALUES
   (12,'Aisha','aisha@fac.com','pig.png', 'true','facebook.com') ,
   (13,'Yahia','yahia@fac.com','cow.png', 'true', 'facebook.com');
 
-INSERT INTO games (name, category, genres, games_modes, cover, first_release_date) VALUES ("Overwatch", 0, [2], [5,12,15], 1464048000000, {
+INSERT INTO games (name, category, genres, game_modes, cover, first_release_date) VALUES ('Overwatch', 0, '[2]', '[5,12,15]', '{
             "url": "//images.igdb.com/igdb/image/upload/t_thumb/yob05bs5ffigu3rmrni1.jpg",
             "cloudinary_id": "yob05bs5ffigu3rmrni1",
             "width": 1059,
             "height": 1500
-        });
+        }',  1464048000000);
 
 COMMIT;
