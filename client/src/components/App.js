ecac5ba817f4269fbaa936c60a5b697d68405e01
mineshmshah <mineshmshah@hotmail.co.uk>
2017-10-12 01:23:29 +0100

    Set up front end react servers and proxies 32 #32 #33

---
+++ client/src/components/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+// Browser router tells react router how to behave - what components are to be visible by looking at URL
+// Route deals with the rules and decides what components to show
+import { BrowserRouter , Route } from 'react-router-dom';
+
+const Header = ()=><h2>Header</h2>;
+const Form = ()=><h2>Form</h2>;
+const Profile = ()=><h2>Profile</h2>;
+const Landing = ()=><h2>Landing</h2>;
+
+//functional component
+const App =() =>{
+	return(
+		<div>
+			<BrowserRouter>
+				<div>
+					<Header />
+					<Route exact={true} path="/" component={Landing} />
+					<Route  path="/welcome" component={Form} />
+					<Route path="/profile" component={Profile}/>
+				</div>
+			</BrowserRouter>
+		</div>
+	);
+};
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import {createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
+//create-react-app assumes there is an index.js and is the root file of the application
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components/App';
+
+const store = createStore(reducers,{},applyMiddleware());
+
+ReactDOM.render(
+	<Provider store={store}><App /></Provider>,
+	document.querySelector('#root')
+);
