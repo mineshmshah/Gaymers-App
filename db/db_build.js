7c620f06f4dcb170d1944b11504fade5f3d9df3f
mineshmshah <mineshmshah@hotmail.co.uk>
2017-12-30 02:08:09 +0000

    updated form changes

--- db/db_build.js
+++ db/db_build.js
@@ -5,8 +5,8 @@ const dbConnection = require('./db_connections.js');
 const sql = fs.readFileSync(`${__dirname}/db_build.sql`).toString();
 
 const runDbBuild = dbConnection.query(sql, (err, res) => {
-    if (err) throw err;
-    console.log("Gaymers directory created with result: ", res);
+  if (err) throw err;
+  console.log("Gaymers directory created with result: ", res);
 });
 
 module.export = runDbBuild;
